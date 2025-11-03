b5084d1073dc4b82aeeb79056e56ddbe4830dc4d
Joel Calado <joelcalado@gmail.com>
2025-11-03 07:15:01 +0000

    Bump version to 0.1.2

--- rt4d_codeplug/__init__.py
+++ rt4d_codeplug/__init__.py
@@ -3,7 +3,7 @@
 A Python library and GUI for parsing, editing, and flashing RT-4D radio codeplug files.
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .models import (
     Channel, Contact, GroupList, Zone, Codeplug, RadioSettings,
